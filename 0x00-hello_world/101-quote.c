6ef521d9f25230b812af20be71cc6ebab642c41f
estherariwayo <ariwayo33@gmail.com>
2023-02-19 21:15:35 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that prints a line to the standard error
+ * Return: 1 (success)
+ */
+int main(void)
+{
+fput("and tbat piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+return (1);
