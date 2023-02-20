d1cf0cf13442bf69b644c3ed06f65435f5d51568
estherariwayo <ariwayo33@gmail.com>
2023-02-19 21:30:50 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-fputs("and tbat piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
 return (1);
 }
